54faa60011a416ae68744ebd35ee34b010484995
Enrique Pando <enr.pando@gmail.com>
2021-04-27 10:07:37 -0500

    Agregar diagrama

--- config/header.php
+++ config/header.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: http://localhost:8080');
+header('Access-Control-Allow-Origin: http://localhost:3000');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 
