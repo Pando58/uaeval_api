57c7252ac1968f26b4849c1578befc7848133d7a
Enrique Pando <enr.pando@gmail.com>
2021-05-07 13:38:32 -0500

    Arreglar error por caracteres al obtener de la API

--- clases/cRecurso.php
+++ clases/cRecurso.php
@@ -97,9 +97,13 @@ class Recurso {
     $res = [];
 
     while($row = $stmt->fetch(PDO::FETCH_OBJ)) {
-      array_push($res, $row);
-    }
+      $obj = [];
+      foreach ($row as $key => &$val) {
+        $obj[$key] = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $val);
+      }
 
+      array_push($res, $obj);
+    }
     return $res;
   }
 
